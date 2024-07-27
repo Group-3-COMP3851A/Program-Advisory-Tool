566910426c39faf2fdc6c699f1cb4cd40976ed8d
Griffin Mitchell <griffin80567@gmail.com>
2024-07-27 19:09:55 +1000

    basic functionality for getting a student information based on student ID from the database

--- backend/dao/studentDAO.js
+++ backend/dao/studentDAO.js
@@ -1,1 +1,42 @@
 //File for accessing the database.
+import mongodb from "mongodb";
+
+const ObjectId = mongodb.ObjectId;
+
+let students;
+
+export default class studentsDAO{
+    static async injectDB(conn) {
+        if (students) return //if there is already a connection, skip this
+        try {
+            students = await conn.db("ProgramAdvisoryTool").collection("students"); //querying students collection
+        } catch (e) {
+            console.error(`Unable to establish collection name in moviesDAO, you probably named the collection wrong: ${e}`);
+        }
+    }
+
+    static async getStudent({
+        studentNo 
+    } = {}) {
+        let query; //the query that will be used to query the database
+        query = {"_id": studentNo};
+
+        let cursor; //cursor is essentially just query results
+
+        try {
+            cursor = await students.find(query); //query the db
+        } catch (e) {
+            console.error(`Query could not be completed: ${e}`); //error
+            return {studentInfo: []}
+        }
+
+        try {
+            const studentInfo = await cursor.toArray(); //convert the student info to an array
+            return {studentInfo}; //return the result
+        } catch (e) {
+            console.error(`Unable to convert to Array: ${e}`);
+            return {studentInfo: []};
+        };
+    };
+    
+};
