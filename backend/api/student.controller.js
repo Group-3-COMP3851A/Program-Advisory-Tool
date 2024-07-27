2c49ff8d1c796c76749fa9f18438a9f0f69cdede
Griffin Mitchell <griffin80567@gmail.com>
2024-07-27 20:54:21 +1000

    commenting for TODO

--- backend/api/student.controller.js
+++ backend/api/student.controller.js
@@ -19,10 +19,14 @@ export default class studentCtrl {
 
     static async apiLogin (req, res, next) {
         const studentNo = parseInt(req.query.studentNo);
+
+        //TODO: HASH THE INPUT PASSWORD
         const inputPassword = req.query.password; //this will be hashed later, simply a test for now.
 
         let password = await studentDAO.getHashedPassword({studentNo}); //retrieving the password from the db
 
+        /* this shouldn't really return just true or false, it should probably return a token or something
+        that should be used to authorise a user's requests instead of username/password */
         if (inputPassword == password) res.send(true);
         else res.send(false);
     }
