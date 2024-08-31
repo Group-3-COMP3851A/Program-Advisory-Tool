d94cb4df78c3c808d1ea9a994240a4cb001986ae
KewlKid88 <c3367187@uon.edu.au>
2024-08-31 22:29:21 +1000

    Added materialUI to the card component

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,14 +1,30 @@
-import React from 'react'; // Importing React to use JSX and other React features
-import { useDrag } from 'react-dnd'; // later use
+import React from 'react';
+import { useDrag } from 'react-dnd'; //For later use
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const Card = ({ text }) => { // Defining a functional component named Card that accepts text as a prop
+export default function OutlinedCard(props) {
   return (
-    <div style={cardStyle}> {} 
-      {text} {/* Displaying the text passed as a prop inside the card */}
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+		  {props.courseCode};
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+			  {props.courseName};
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
-};
+}
 
+/* Old card styling
 const cardStyle = { // Defining a constant object cardStyle to hold the CSS styles for the card
   padding: '8px', // Setting padding inside the card to create space around the text
   margin: '4px', // Setting margin outside the card to create space between this card and others
@@ -19,6 +35,7 @@ const cardStyle = { // Defining a constant object cardStyle to hold the CSS styl
 };
 
 export default Card; // Exporting the Card component as the default export
+*/
 
 
 
