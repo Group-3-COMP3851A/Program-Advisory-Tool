223c496f6d27974b18181e6c87aab20597363f51
Harry-Ricketts <100877593+Harry-Ricketts@users.noreply.github.com>
2024-09-01 21:18:03 +1000

    Update Course List API Function
    I have no clue if this is even necessary, but this returns all the data I need, in a format which is usable :/

--- backend/api/controllers/algorithm.controller.js
+++ backend/api/controllers/algorithm.controller.js
@@ -12,11 +12,15 @@ export default class algorithmCtrl {
             const directedObject = await algorithmDAO.getDirectedPlaceholders(major);
             const completedCourses = []; // await algorithmDAO.getCompletedCourses(studentId);
 
-            // Pass course list into algorithm handler, and return sorted course list
-            // TODO: Maybe wrap the handler in its own algorithm object?
-
-            // TODO: Retrieve the semesterCount and the coursesPerSem from the select page, instead of hardcoding
             const handler = new AlgorithmHandler(courseList, directedObject, completedCourses, semCount, coursesPerSem);
+
+            const coursePlan = handler.planSchedule;
+
+            const courseMap = new Map(courseList.map(course => [course._id, course]));
+
+            const schedule = coursePlan.map(semester => semester.map(course => courseMap.get(course.code) || course));
+
+            //console.log(schedule);
             
             // I'll update the response structure once I get everything working correctly
             let response = {
