97fe819c39d8104d6e63aaf1f7ce66f509c9e7b4
Griffin Mitchell <griffin80567@gmail.com>
2024-09-11 05:09:16 +0100

    added units of study checking. Once starting in semester 2 becomes an option, it will need to be used here. Completed courses will also need to be sent here.

--- backend/api/functions/courseMissingDependencyCheck.js
+++ backend/api/functions/courseMissingDependencyCheck.js
@@ -1,7 +1,7 @@
 /**
  * Checks if the given schedule has any missing dependencies.
- * @param {Object[][]} schedule - a 2D array of courses, where each inner array represents a semester. Each course has the form: course: {code: courseCode, prio: priority, index: index in courseCodeList}
- * @param {string[]} changedCourses - an array of course codes, representing the courses to be checked
+ * @param {Object[][]} schedule - a 2D array of courses, where each inner array represents a semester. Each course has the form: course: {code: courseCode, prio: priority, index: index in courseCodeList}. This comes from the client request
+ * @param {string[]} changedCourses - an array of course codes, representing the courses to be checked. This comes from the client request
  * @param {Object} dependencyGraph - an adjacency list of courses, where the key is the course and the value is an array of its dependencies. This comes from the algorithmHandler/algorithm
  * @param {string[]} courseArray - an array of all courses. Comes from the algorithm/handler.
  * @returns {boolean | string[][]} false if there are no missing dependencies, a list of missing dependencies otherwise. If returning conflicts, each element is the list of warnings or errors for the given course. Each warning or error can be determined by the 3 letters before the space in the returned string.
@@ -35,21 +35,23 @@ const courseMissingDependencyCheck = (schedule, changedCourses, dependencyGraph,
                     earlyChangedCourseIndex = changedCourses.indexOf(course.code);
                 }
                 changedCourseIndexes.push([changedCourses.indexOf(course.code), course]);
+                //checking semester offering
                 let semester_offered = courseArray[course.index].semester_offered;
                 if ((!(semester_offered === 0))&&((semester_offered+ i) % 2 === 0)) {
                     //the course will either be the first course added or the second and since they are pushed not set, can just check the length of the array
                     if (changedCourseIndexes.length === 1) conflicts[changedCourseIndexes[0][0]].push("sem");
                     else conflicts[changedCourseIndexes[1][0]].push("sem");
                 }
+                if (course.us) {
+                    let currentUnits = i * semester.length; //if the plan started in semester 2, will need to account for that. Will also need to account for the completed courses.
+                    if (currentUnits < course.us) {
+                        if (changedCourseIndexes.length === 1) conflicts[changedCourseIndexes[0][0]].push("uos");
+                        else conflicts[changedCourseIndexes[1][0]].push("uos");
+                    }
+                }
             }
         }
     }  
-
-    //UOS checking
-    changedCourseIndexes.forEach(changedCourseIndex => {
-        if (changedCourseIndex[1].us) conflicts[changedCourseIndex[0]].push("uos")
-    })
-
 
     let dependencies = structuredClone(dependencyGraph[earlyChangedCourse.index]);
 
@@ -75,10 +77,10 @@ const courseMissingDependencyCheck = (schedule, changedCourses, dependencyGraph,
         });
     }
 
+    //checking course following stuff
     changedCourses.forEach((courseCode, index) => {
         for (let i = 0; i < courseArray.length; i++) {
             const course = courseArray[i];
-            //course.follows needs to be whatever it is called from the database
             if (course._id === courseCode && course.course_follow !== "") conflicts[index].push("fol "+courseCode); //check if the course has a follower course
             if (course.course_follow === courseCode) conflicts[index].push("fol "+courseCode); //check if the course is a follower course
         }
