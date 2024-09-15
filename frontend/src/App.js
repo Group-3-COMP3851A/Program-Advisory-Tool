166fb083df857a1bd412eb2114b401552e790161
KewlKid88 <c3367187@uon.edu.au>
2024-09-15 23:06:45 +1000

    Further small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Select from './pages/Select';
 import Profile from './pages/Profile';
-import ViewPlanner from './pages/ViewPlanner';
 import Logout from './pages/Logout';
-import Help from './pages/Help';
 import AddElectives from './pages/AddElectives';
-import Completed from './pages/Completed';
 import GeneratePlan from './pages/GeneratePlan';
 import { AppProvider } from './AppContext';
 function App() {  
@@ -216,10 +213,7 @@ return (
     <Routes>
     <Route path="/" element={<Select />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/view-planner" element={<ViewPlanner />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/completed" element={<Completed />} />
         <Route path="/add-electives" element={<AddElectives />} />
         <Route path="/generate-plan" element={<GeneratePlan/>}/>
         <Route path="/select" element={<Select/>}/>
--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -9,11 +9,11 @@ function Menu() {
 		<div className="menu">
 			<img src={logo} alt="UoN Logo"/>
 			<ul>
+			    <li><Link to="/select" text="Create new planner"/></li>
                 <li><Link to="/profile" text="Profile" /></li>
                 <li><Link to="/view-planner" text="View Planner" /></li>
                 <li><Link to="/logout" text="Logout" /></li>
                 <li><Link to="https://askuon.newcastle.edu.au/" text="Ask Uon" external /></li>
-                <li><Link to="/select" text="Create new planner"/></li>
 			</ul>
 		</div>
 	);
