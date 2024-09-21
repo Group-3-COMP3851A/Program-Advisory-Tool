d0c89620ff352d943eafd1209fa3efeb2c12fae9
KewlKid88 <c3367187@uon.edu.au>
2024-09-21 16:39:36 +1000

    Help icon added

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,6 +4,7 @@ import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { AppContext } from '../AppContext';
+import Popover from '@mui/material/Popover';
 
 export const OutlinedCard = ({text, ...props}) => {
 
@@ -168,19 +169,6 @@ function getCourseURL(courseId)
 
   return "https://www.newcastle.edu.au/course/" + courseCode;
 }
-
-/* Old card styling
-const cardStyle = { // Defining a constant object cardStyle to hold the CSS styles for the card
-  padding: '8px', // Setting padding inside the card to create space around the text
-  margin: '4px', // Setting margin outside the card to create space between this card and others
-  backgroundColor: '#f0f0f0', // Setting the background color of the card to a light gray
-  border: '1px solid #ccc', // Adding a solid border with a light gray color
-  borderRadius: '4px', // Rounding the corners of the card
-  textAlign: 'center', // Centering the text horizontally within the card
-};
-
-export default Card; // Exporting the Card component as the default export
-*/
 
 
 
