c457fc43f79a9c6a3b89b3de7b179bb26fba3fa9
Harry-Ricketts <100877593+Harry-Ricketts@users.noreply.github.com>
2024-09-23 15:23:24 +1000

    Update DnD Help Text

--- frontend/src/pages/Plan.js
+++ frontend/src/pages/Plan.js
@@ -93,13 +93,13 @@ const Plan = () => {
         <div className='global'>
             <div className='gen-section'>
                 <Text type="h1" className='page-title'>This program plan is for an student in the {degree} with a major in {major} modify with any selected completed courses which are place at the bottom.</Text>
+                <div><Text type="h2" className='S1'>Each blue box represents a semester. When you hover your mouse over it, it highlights to display the courses for that semester. If you hover over a specific course, it will highlight the essential information you need to know.</Text></div>
                 <HelpIcon text1="to swap courses click and drap it to another semester and drop it on the first course in that semester"
 					text2="4 Courses is considered full-time study."/>
                 <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
                     {courseList.map((year, yearIndex) => (
                         <div key={yearIndex} className='year-container'>
                             <Text type="h2" className='year-title'>Year {yearIndex + 1}</Text>
-                            <div><Text type="h2" className='S1'>Each blue box represents a semester. When you hover your mouse over it, it highlights to display the courses for that semester. If you hover over a specific course, it will highlight the essential information you need to know.</Text></div>
                             <div className='semester-container'>
                                 {year.map((semester, semesterIndex) => (
                                     <div key={semesterIndex} className='semester-section'>
