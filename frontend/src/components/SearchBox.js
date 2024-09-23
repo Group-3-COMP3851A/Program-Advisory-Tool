6fde2a163f97b724e01319120379a6106fad0740
Harry-Ricketts <100877593+Harry-Ricketts@users.noreply.github.com>
2024-09-23 13:16:23 +1000

    Add Course Code to Completed Course List

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -11,7 +11,7 @@ export default function MultiSearchBox(props) {
         multiple
         value={value}
         options={options}
-        getOptionLabel={(option) => option.course_name}
+        getOptionLabel={(option) => getFullCourseCode(option._id) + " - " + option.course_name}
         onChange={(event, newValue) => {
           onChange(newValue);
         }}
@@ -21,6 +21,38 @@ export default function MultiSearchBox(props) {
       />
     </div>
   );
+}
+
+function getFullCourseCode(internalId)
+{
+  let courseString = String(internalId);
+  let courseId = courseString.slice(-2);
+  let courseName = courseString.slice(0, -2);
+  let fullName = "";
+
+  switch(courseId)
+  {
+    // We can add extra cases if extra courses are added to the database
+    case "co":
+      fullName = "COMP";
+      break;
+    case "se":
+      fullName = "SENG";
+      break;
+    case "ma":
+      fullName = "MATH";
+      break;
+    case "in":
+      fullName = "INFT";
+      break;
+    case "el":
+      fullName = "ELEC";
+      break;
+    default:
+      fullName = courseId;
+  }
+
+  return fullName + courseName;
 }
 
 //The main degree select bar
