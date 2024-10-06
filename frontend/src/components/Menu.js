889de36f1a802e5e7d0c4709d3d38763b64ce2ad
Muhammad Qasim <qasimsh89@gmail.com>
2024-10-06 20:16:36 +1100

    -> worked on responsive design of the select page and plan page. -> fix the logout menu button also edited the menu.js as well as the css -> removed the un-used css and cleaned up the style.css file -> remove all the media tags from css and use sinfle media tag for dedicated aspect ratio

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -4,18 +4,20 @@ import Link from './Link.js';
 import '../styles/style.css';
 
 //Written by Jarod 22/08
-
+//edited by Muhammad 06/10/24
 function Menu() {
 	return(
 		 <div className="menu">
       <img src={logo} alt="UoN Logo" className="menu-logo" />
+      
       <ul className="menu-list">
         <li><Link to="/profile" text="Profile" /></li>
         <li><Link to="https://askuon.newcastle.edu.au/" text="Ask Uon" external /></li>
-    
         <li><Link to="/select" text="Create New Planner" /></li>
-        <li><Link to="/logout" text="Logout"/></li>
       </ul>
+       <ul className='logout' >
+            <li><Link to="/logout"text="Logout" /> </li>
+        </ul>
     </div>
   );
 }
