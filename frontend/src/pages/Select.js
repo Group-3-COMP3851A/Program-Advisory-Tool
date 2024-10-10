1d496be4afab8bbad49a3de38fe43161b50f67e4
Griffin Mitchell <griffin80567@gmail.com>
2024-10-10 02:27:59 +0100

    fix warning with useEffect dependency

--- frontend/src/pages/Select.js
+++ frontend/src/pages/Select.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useMemo } from 'react';
 import '../styles/style.css';
-import Menu from '../components/Menu';
 import Dropdown from '../components/Dropdown';
 import { SearchBox } from '../components/SearchBox';
 import Text from '../components/Text';
@@ -20,12 +19,19 @@ const Select = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
 
-    const coursesPerSemOptions = [{ value: '2', label: '2' }, { value: '3', label: '3' }, { value: '4', label: '4' }];
+    const coursesPerSemOptions = useMemo(
+        () => [
+            { value: "2", label: "2" },
+            { value: "3", label: "3" },
+            { value: "4", label: "4" },
+        ],
+        []
+    );
 
     useEffect(() => {
         getDegreeList();
         setDropdownOptions(coursesPerSemOptions);			
-    }, []);
+    }, [coursesPerSemOptions]);
 
     const showMajor = (selectedDegree) => {
 		setDegree(selectedDegree);
@@ -163,9 +169,3 @@ const Select = () => {
 };
 
 export default Select;
-
-
-
-
-
-
